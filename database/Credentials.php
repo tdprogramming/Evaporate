54b46fa5bdd0d8ff172cad149080cc4ab0f41822
tdprogramming <timdiggle@tdprogramming.com>
2016-10-09 07:40:49 -0700

    Blank out credentials again
    Blank out creds

--- database/Credentials.php
+++ database/Credentials.php
@@ -1,9 +1,9 @@
 <?php
 
 class Credentials {
-    public static $dbHost = "hjoiuoi";       // Add your database host here
-    public static $dbUserName = "oiu";   // Add your username here
-    public static $dbPassword = "uu";   // Add your password here
-    public static $dbName = "uu";       // Add your database name here
-    public static $sessionFingerprint = "ooo";     // Add a random string here for the session fingerprint
+    public static $dbHost = "";       // Add your database host here
+    public static $dbUserName = "";   // Add your username here
+    public static $dbPassword = "";   // Add your password here
+    public static $dbName = "";       // Add your database name here
+    public static $sessionFingerprint = "";     // Add a random string here for the session fingerprint
 }
