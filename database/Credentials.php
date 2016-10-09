0eafb1229018635b056e561fcbcb2e8be23e325f
tdprogramming <timdiggle@tdprogramming.com>
2016-10-09 08:04:25 -0700

    Syntax fix
    Fix syntax for public consts in Credentials.php

--- database/Credentials.php
+++ database/Credentials.php
@@ -1,9 +1,9 @@
 <?php
 
 class Credentials {
-    public static $dbHost = "";       // Add your database host here
-    public static $dbUserName = "";   // Add your username here
-    public static $dbPassword = "";   // Add your password here
-    public static $dbName = "";       // Add your database name here
-    public static $sessionFingerprint = "";     // Add a random string here for the session fingerprint
+    const dbHost = "";       // Add your database host here
+    const dbUserName = "";   // Add your username here
+    const dbPassword = "";   // Add your password here
+    const dbName = "";       // Add your database name here
+    const sessionFingerprint = "";     // Add a random string here for the session fingerprint
 }
