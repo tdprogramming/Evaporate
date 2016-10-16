8889248b279524de64f204ce1ebe133f7519cd15
tdprogramming <timdiggle@tdprogramming.com>
2016-10-16 09:43:11 -0700

    Added UserManager Stub
    Added user manager stub and credentials member for root email address

--- database/Credentials.php
+++ database/Credentials.php
@@ -6,4 +6,5 @@ class Credentials {
     const dbPassword = "";   // Add your password here
     const dbName = "";       // Add your database name here
     const sessionFingerprint = "";     // Add a random string here for the session fingerprint
+    const automatedEmail = "";      // An email address to show on the from and reply-to headers for things like forgotten password emails
 }
